4b83ff77c5a369eb8927c8cd47bc4bcc28fe0770
sue-CC <caishutong26@gmail.com>
2024-06-21 01:25:44 +0200

    create catalog server

---
+++ microservice-kubernetes-demo/microservice-kubernetes-demo-catalog/src/main/java/com/ewolff/microservice/catalog/grpc/server/CatalogGrpcServerConfiguration.java
@@ -0,0 +1,28 @@
+package com.ewolff.microservice.catalog.grpc.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class CatalogGrpcServerConfiguration {
+
+    @Autowired
+    private CatalogServiceImpl catalogService;
+
+    @Bean
+    public Server grpcServer() throws IOException {
+        // bind the port
+        Server server = ServerBuilder.forPort(8080)
+                .addService(catalogService) // publish service
+                .build(); // create server object
+        server.start();
+        System.out.println("the listitems:" + server.getServices());
+        System.out.println("Catalog gRPC Server started, listening on " + server.getPort());
+        return server;
+    }
+}
